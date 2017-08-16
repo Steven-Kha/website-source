76653075da0f4018254bd4bbcf808b68f015bcd9
Steven Kha <stevenvkha@gmail.com>
2017-08-16 14:08:00 -0700

    let see what new div and img works

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div> 
       </div>
     );
   }
