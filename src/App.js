c1ff672ffc5495bb9184ad481ea593fc2d139ab2
Steven Kha <stevenvkha@gmail.com>
2017-09-03 20:08:05 -0700

    did this fix it yet?

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Steven Kha's Website! Work in Progress!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to Steven Kha's Website! Work in Progress!</h2>
+                </div>
+                <p className="App-intro">
+                    Welcome to my site:
+                </p>
+                <p>
+                    Does this also make a new line in between?
+                </p>
+                <p>
+                    Yes it does! Testing image below...
+                </p>
+                <p>
+                    <img alt = "cool logo" src = "https://upload.wikimedia.org/wikipedia/fr/thumb/8/87/The_Legend_of_Zelda_Breath_of_the_Wild_logo.png/1280px-The_Legend_of_Zelda_Breath_of_the_Wild_logo.png"
+                         height = "251" width = "320"/>
+                </p>
+                <br />
+                <p> New Line goes here: </p>
+
+            </div>
+        );
+    }
 }
 
 export default App;
