766c670f8efe36648de400b53c99e2393005b9aa
Steven Kha <stevenvkha@gmail.com>
2017-09-03 20:03:34 -0700

    added image back in

--- src/App.js
+++ src/App.js
@@ -8,24 +8,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Steven Kha's Website!</h2>
+          <h2>Welcome to Steven Kha's Website! Work in Progress!</h2>
         </div>
         <p className="App-intro">
-          Welcome to my site: It's a work in progress
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-          <p>
-              Does this also make a new line in between?
-          </p>
-          <p>
-              Yes it does! Testing image below...
-          </p>
-          <p>
-              <img alt = "cool logo" src = "https://upload.wikimedia.org/wikipedia/fr/thumb/8/87/The_Legend_of_Zelda_Breath_of_the_Wild_logo.png/1280px-The_Legend_of_Zelda_Breath_of_the_Wild_logo.png"
-                   height = "251" width = "320"/>
-          </p>
-          <br />
-              <p> New Line goes here, yeah: </p>
-
       </div>
     );
   }
